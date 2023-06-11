de20ae36180e7944dc56f1d00a2f24dfdafc6dd3
Nishu Singh <nishu.arch@gmail.com>
2023-06-11 14:39:31 +0530

    Removed console.log() function used for debugging

--- Assignment.js
+++ Assignment.js
@@ -6,25 +6,19 @@ for (list of lists) {
   list.addEventListener("dragstart", (e) => {
     let selected = e.target;
 
-    console.log("dragstart has been trigger");
-
     rightBox.addEventListener("dragover", (e) => {
       e.preventDefault();
-      console.log("dragover has been trigger");
     });
     rightBox.addEventListener("drop", (e) => {
       rightBox.appendChild(selected);
       selected = null;
-      console.log("drop has been trigger");
     });
     leftBox.addEventListener("dragover", (e) => {
       e.preventDefault();
-      console.log("dragover has been trigger");
     });
     leftBox.addEventListener("drop", (e) => {
       leftBox.appendChild(selected);
       selected = null;
-      console.log("drop has been trigger");
     });
   });
 }
